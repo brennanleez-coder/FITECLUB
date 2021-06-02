5dc835a53aeac540f63cf60c128e9f0b3dd4ba77
brennanleez-coder <brennanlee95@gmail.com>
2021-06-03 01:41:49 +0800

    Bottom tab updated

--- routes/NestedHomeStack.js
+++ routes/NestedHomeStack.js
@@ -1,8 +1,5 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import Home from '../screens/Home';
-import Login from '../screens/Login';
-import NestedHome from '../screens/NestedHome'
 
 import Search from '../screens/Search'
 import Profile from '../screens/Profile'
--- screens/NestedHome.js
+++ screens/NestedHome.js
@@ -1,18 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { ImageBackground,
-  StyleSheet, Button, View, SafeAreaView, Text, Alert, Image, ScrollView } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import NestedHome from '../screens/NestedHome'
 import Profile from '../screens/Profile'
 import Search from '../screens/Search'
 import Rankings from '../screens/Ranking'
 
 const Tab = createBottomTabNavigator();
-
-export const Home = ({navigation}) => {
+const Home = ({navigation}) => {
   return (
       <Tab.Navigator>
           <Tab.Screen name="Search" component={ Search} />
@@ -21,25 +17,5 @@ export const Home = ({navigation}) => {
       </Tab.Navigator>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {        
-    flex: 1,
-    justifyContent: 'center',
-
-  },
-  search: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
-  },
-});
 
 export default Home;
