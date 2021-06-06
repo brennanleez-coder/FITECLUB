d49e8e5eed07af01d649efb3a6db64a13246e3b9
brennanleez-coder <brennanlee95@gmail.com>
2021-06-06 13:46:27 +0800

    Integrated Explore Page

--- screens/NestedHome.js
+++ screens/NestedHome.js
@@ -6,11 +6,13 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Profile from '../screens/Profile'
 import Search from '../screens/Search'
 import Rankings from '../screens/Ranking'
+import Explore from '../screens/Slider'
 
 const Tab = createBottomTabNavigator();
 const Home = ({navigation}) => {
   return (
       <Tab.Navigator>
+          <Tab.Screen name="Explore" component={ Explore } />
           <Tab.Screen name="Search" component={ Search} />
           <Tab.Screen name="Profile" component={ Profile } />
           <Tab.Screen name="View Rankings" component={ Rankings } />
