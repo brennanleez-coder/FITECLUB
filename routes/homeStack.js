76ea67614995bff2b7ebf1c746279f5f31020a73
brennanleez-coder <brennanlee95@gmail.com>
2021-06-16 23:25:16 +0800

    Update homeStack.js

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,5 +1,7 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import StartPage from '../screens/StartPage'
+import SignUp from '../screens/SignUp'
 import SignIn from '../screens/SignIn'
 import NestedHome from '../screens/NestedHome'
 
